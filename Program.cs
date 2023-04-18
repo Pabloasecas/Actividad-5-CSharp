e5d3155741866c557cbd5091ebda4dc0844b300d
Pablo <pablogcg05@gmail.com>
2023-04-18 12:36:44 +0200

    Adición bucle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿Console.WriteLine("Hola, introduzca su nombre");
 var nombre = Console.ReadLine();
+Console.Write("Adiós ");
+for (int i = (nombre.Length-1); i >= 0; i--)
+{
+    Console.Write(nombre[i]);
+}
